1ce1ba8413d0efe8aa96d6afe85c2be5689cb742
Moon Kim <Jkim5918@conestogac.on.ca>
2019-10-24 21:08:37 -0400

    edit menu list

---
+++ Models/Cart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VirtualGameStore.Models
+{
+    public partial class Cart
+    {
+        public decimal? Quantity { get; set; }
+
+        public Game Game { get; set; }
+    }
+}
